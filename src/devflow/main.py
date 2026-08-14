992aa62eae308df0eff5b762a989bdbb4f1a56e7
Patrick <patrickcabrera2025@gmail.com>
2026-08-14 15:33:00 -0500

    feat: register auth router in the application

--- src/devflow/main.py
+++ src/devflow/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+from devflow.app.routers import auth_router
+
 app = FastAPI(title='devflow')
+
+app.include_router(auth_router)
 
 @app.get('/health')
 def check_health():
